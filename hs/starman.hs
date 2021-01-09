e1bbc56960aa673b222aafeb29447f82eae0bf9e
Christos Gogos <chgogos@gmail.com>
2021-01-10 00:08:45 +0200

    hs

---
+++ hs/starman.hs
@@ -0,0 +1,25 @@
+check :: String -> String -> Char -> (Bool, String)
+check word display c
+  = (c `elem` word, [if x==c
+          then c
+          else y | (x,y) <- zip word display])
+
+turn :: String -> String -> Int -> IO ()
+turn word display n =
+  do if n==0
+       then putStrLn "You lose"
+       else if word==display
+              then putStrLn "You win!"
+              else mkguess word display n
+
+
+mkguess word display n =
+  do putStrLn (display ++ "  " ++ take n (repeat '*'))
+     putStr "  Enter your guess: "
+     q <- getLine
+     let (correct, display') = check word display (q!!0)
+     let n' = if correct then n else n-1
+     turn word display' n'
+
+starman :: String -> Int -> IO ()
+starman word n = turn word ['-' | x <- word] n

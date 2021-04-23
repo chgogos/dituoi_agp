918d99a24088546fe5581637c88e51dbd3033a2e
Christos Gogos <chgogos@gmail.com>
2021-04-23 12:25:35 +0300

    minor

--- pl/python/scope1b.py
+++ pl/python/scope1b.py
@@ -2,7 +2,7 @@ day = "Monday"
 
 
 def tester():
-    print(f"The day is {day}")  # πρόσβαση στην global μεταβλητή day
+    print(f"The day is {day}")  # πρόσβαση στην global μεταβλητή day (UnboundLocalError: local variable 'day' referenced before assignment )
     day = "Tuesday"
     print(f"The day is {day}")  # πρόσβαση στην global μεταβλητή day
 
